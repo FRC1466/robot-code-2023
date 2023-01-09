f0e85310b3d95fc6dbe8ae6c14afe156faee0125
webbrobotics <45609921+webbrobotics@users.noreply.github.com>
2023-01-09 18:21:48 -0500

    fix trajectory error
    - initialize command inside of class initializer

--- main/java/frc/robot/commands/Autonomous/SwerveTrajectory.java
+++ main/java/frc/robot/commands/Autonomous/SwerveTrajectory.java
@@ -14,6 +14,7 @@ import frc.robot.Constants.DriveConstants;
 
 public class SwerveTrajectory {
     private DriveSubsystem m_drive;
+    private PPSwerveControllerCommand m_swerveControllerCommand;
 
     PathPlannerTrajectory examplePath = PathPlanner.loadPath("Test Path", new PathConstraints(4, 3));
 
@@ -25,32 +26,33 @@ public class SwerveTrajectory {
             AutoConstants.THETA_CONTROLLER_CONSTRAINTS);
     
     public SwerveTrajectory(DriveSubsystem drive) {
+
         m_drive = drive;
         thetaController.enableContinuousInput(-Math.PI, Math.PI);
+
+        m_swerveControllerCommand =
+        new PPSwerveControllerCommand(
+            examplePath,
+            m_drive::getPose, // Functional interface to feed supplier
+            DriveConstants.KINEMATICS,
+            // Position controllers
+            new PIDController(
+                AutoConstants.TRANSLATION_CONTROLLER.P,
+                AutoConstants.TRANSLATION_CONTROLLER.I, 
+                AutoConstants.TRANSLATION_CONTROLLER.D),
+            new PIDController(
+                AutoConstants.TRANSLATION_CONTROLLER.P, 
+                AutoConstants.TRANSLATION_CONTROLLER.I, 
+                AutoConstants.TRANSLATION_CONTROLLER.D),
+            new PIDController(
+                AutoConstants.THETA_CONTROLLER.P,
+                AutoConstants.THETA_CONTROLLER.I, 
+                AutoConstants.THETA_CONTROLLER.D),
+            m_drive::driveFromModuleStates,
+            m_drive);
     }
 
     // look into auto builder and markers
-
-    PPSwerveControllerCommand m_swerveControllerCommand =
-            new PPSwerveControllerCommand(
-                examplePath,
-                m_drive::getPose, // Functional interface to feed supplier
-                DriveConstants.KINEMATICS,
-                // Position controllers
-                new PIDController(
-                    AutoConstants.TRANSLATION_CONTROLLER.P,
-                    AutoConstants.TRANSLATION_CONTROLLER.I, 
-                    AutoConstants.TRANSLATION_CONTROLLER.D),
-                new PIDController(
-                    AutoConstants.TRANSLATION_CONTROLLER.P, 
-                    AutoConstants.TRANSLATION_CONTROLLER.I, 
-                    AutoConstants.TRANSLATION_CONTROLLER.D),
-                new PIDController(
-                    AutoConstants.THETA_CONTROLLER.P,
-                    AutoConstants.THETA_CONTROLLER.I, 
-                    AutoConstants.THETA_CONTROLLER.D),
-                m_drive::driveFromModuleStates,
-                m_drive);
     
 
     public PPSwerveControllerCommand getCommand() {
