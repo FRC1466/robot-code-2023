eca68e9fb3d205ffb31152119e791d6990534fbd
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-01-26 12:27:33 -0500

    update apriltag field with current game

--- main/java/frc/robot/subsystems/PhotonCameraWrapper.java
+++ main/java/frc/robot/subsystems/PhotonCameraWrapper.java
@@ -29,7 +29,7 @@ public class PhotonCameraWrapper {
 
     public PhotonCameraWrapper() {
         try {
-            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.kDefaultField.m_resourceFile);
+            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
