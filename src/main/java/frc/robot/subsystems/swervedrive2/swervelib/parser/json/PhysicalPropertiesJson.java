2c623dd936838edaef5f554b1785d7e56779ede4
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-02-12 17:45:46 -0500

    add optional json args

--- src/main/java/frc/robot/subsystems/swervedrive2/swervelib/parser/json/PhysicalPropertiesJson.java
+++ src/main/java/frc/robot/subsystems/swervedrive2/swervelib/parser/json/PhysicalPropertiesJson.java
@@ -19,14 +19,14 @@ public class PhysicalPropertiesJson {
   /** Encoder pulse per rotation for non-integrated encoders. 1 for integrated encoders. */
   public MotorConfigInt encoderPulsePerRotation;
   /** The current limit in AMPs to apply to the motors. */
-  public MotorConfigInt currentLimit;
+  public MotorConfigInt currentLimit = new MotorConfigInt(40, 20);
   /** The minimum number of seconds to take for the motor to go from 0 to full throttle. */
-  public MotorConfigDouble rampRate;
+  public MotorConfigDouble rampRate = new MotorConfigDouble(0.25, 0.25);
   /**
    * The grip tape coefficient of friction on carpet. Used to calculate the practical maximum
    * acceleration.
    */
-  public double wheelGripCoefficientOfFriction;
+  public double wheelGripCoefficientOfFriction = 1.19;
   /** Angle motor free speed rotations per minute. */
   public double angleMotorFreeSpeedRPM;
 
