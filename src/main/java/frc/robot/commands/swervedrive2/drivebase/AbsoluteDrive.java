c1832b111c0ecc5cf133b07d54d5b4df1cd583e0
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-13 23:28:29 -0600

    Added method to fetch ChassisSpeeds if given x, y, and angle.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/commands/swervedrive2/drivebase/AbsoluteDrive.java
+++ src/main/java/frc/robot/commands/swervedrive2/drivebase/AbsoluteDrive.java
@@ -67,13 +67,10 @@ public class AbsoluteDrive extends CommandBase {
   public void execute() {
 
     // Get the desired chassis speeds based on a 2 joystick module.
-    ChassisSpeeds desiredSpeeds =
-        swerve.getTargetSpeeds(
-            vX.getAsDouble(),
-            vY.getAsDouble(),
-            headingHorizontal.getAsDouble(),
-            headingVertical.getAsDouble(),
-            swerve.getHeading().getRadians());
+
+    ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(),
+                                                         headingHorizontal.getAsDouble(),
+                                                         headingVertical.getAsDouble());
 
     // Limit velocity to prevent tippy
     Translation2d translation = SwerveController.getTranslation2d(desiredSpeeds);
