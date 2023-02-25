2a6bbf0c09176aeaf258d37237efac9ba7187ffd
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-02-24 23:14:17 -0500

    move and rename files

--- src/main/java/frc/robot/commands/swervedrive/AutoMap.java
+++ src/main/java/frc/robot/commands/swervedrive/AutoMap.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.swervedrive2.auto;
+package frc.robot.commands.swervedrive;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
