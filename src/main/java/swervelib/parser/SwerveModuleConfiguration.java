32fe122a8c209af8dc3f3683a486f45ab1b07791
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-03-17 12:47:23 -0400

    fix drive motor ff?

--- src/main/java/swervelib/parser/SwerveModuleConfiguration.java
+++ src/main/java/swervelib/parser/SwerveModuleConfiguration.java
@@ -148,11 +148,11 @@ public class SwerveModuleConfiguration {
   public SimpleMotorFeedforward createDriveFeedforward() {
     double kv = physicalCharacteristics.optimalVoltage / maxSpeed;
     /// ^ Volt-seconds per meter (max voltage divided by max speed)
-    double ka =
+    double ks =
         physicalCharacteristics.optimalVoltage
             / calculateMaxAcceleration(physicalCharacteristics.wheelGripCoefficientOfFriction);
     /// ^ Volt-seconds^2 per meter (max voltage divided by max accel)
-    return new SimpleMotorFeedforward(0, kv, ka);
+    return new SimpleMotorFeedforward(ks, kv, 0);
   }
 
   /**
